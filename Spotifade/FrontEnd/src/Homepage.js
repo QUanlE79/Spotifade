c83c391cf7489b6f801db8588d3985a8c4d54891
Le Quan <lequan2002.ql@gmail.com>
2022-07-28 22:40:39 +0700

    home page song side final

--- Spotifade/FrontEnd/src/Homepage.js
+++ Spotifade/FrontEnd/src/Homepage.js
@@ -1,11 +1,12 @@
-const music = new Audio("C:\\Users\\Admin\\Desktop\\ProjectSE\\Group07\\Spotifade\\FrontEnd\\src\\1.mp3");
+var path = "../src/1.mp3";
+const music = new Audio(path);
 
-let masterPlay = document.getElementById('masterPlay');
+let masterPlay = document.getElementById("masterPlay");
 
-masterPlay.addEventListener('click', ()=>{
-    if(music.paused || music.currentTime <= 0){
-        music.play();
-    }  else{
-        console.log('cc');
-    }
-})
+masterPlay.addEventListener("click", () => {
+  if (music.paused || music.currentTime <= 0) {
+    music.play();
+  } else {
+    music.pause();
+  }
+});
