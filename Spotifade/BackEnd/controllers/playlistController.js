056870b867d7fff3a2853b27dbf30dab3a9e0016
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2022-07-30 21:52:29 +0700

    Create react app

---
+++ Spotifade/BackEnd/controllers/playlistController.js
@@ -0,0 +1,53 @@
+const Playlist=require('../models/PlaylistModel');
+//
+exports.getAllPlaylist = async (req,res,next)=>{
+    try{
+        const playlists= await Playlist.find({}).populate('user');
+        res.status(200).json({
+            status:'success',
+            result:playlists.song.length,
+            data:{playlists}
+        })
+    } catch(error){
+        res.json(error);
+    }
+}
+//create One song
+exports.createOnePlaylist = async (req,res,next)=>{
+    try{
+        const {userId}=req.user;
+        const playlist=await Playlist.create({...req.body,user:userId});
+        res.status(200).json({
+            status:'success',
+            data:{ playlist }
+        })
+    } catch(error){
+        next(error);
+    }
+}
+//Update One song
+exports.updateOneSong = async (req,res,next)=>{
+    try{
+        const {songId}=req.params;
+        const song= await Song.findByIdAndUpdate(songId,{...req.body},{new:true,runValidator:true});
+        res.status(200).json({
+            status:'success',
+            data:{song}
+        })
+    } catch(error){
+        next(error);
+    }
+}
+//Delete One song
+exports.deleteOneSong = async (req,res,next)=>{
+    try{
+        const {songId}=req.params;
+        const song= await Song.findByIdAndDelete(songId);
+        res.status(200).json({
+            status:'success',
+            result:'song has been deleted'
+        })
+    } catch(error){
+        next(error);
+    }
+}
--- Spotifade/BackEnd/models/PlaylistModel.js
+++ Spotifade/BackEnd/models/PlaylistModel.js
@@ -1,18 +1,15 @@
-const mongoose=require('mongoose')
-const user=require("\UserModel")
+const mongoose=require('mongoose');
+const user=require("../models/UserModel");
+const song=require("../models/SongModel");
 const Playlistschema = new mongoose.Schema({
+    name:{type: string, trim:true, required:[true,'Playlist name is required']},
+    Song:[song],
     user:
     {type:mongoose.Schema.Types.ObjectId,
-        required:[true,'Missing user ID'],
-        ref:'User'
-
-    },
-    name:{type: string, trim:true, required:[true,'Song\'s name is required']}, 
-    Song:{
-        type: mongoose.Schema.Types.ObjectId,
-        required:[true,'Playlist cannot empty'],
-        ref:'Song'    
+        ref:'user'
     }
+     
+    
 
-})
+},{timestamps:true})
 module.exports=mongoose.model('Playlist',Playlistschema)   
